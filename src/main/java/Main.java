435a12e8c9ff23e94f1e8e7ef236d905e593ad73
Charly Breadberry <aleks_inter@mail.ru>
2021-01-15 20:30:12 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            System.out.println("Ok");
+        }
+    }
+}
---
+++ src/test/resources/test.sql
@@ -0,0 +1,5 @@
+insert into users (name, age, email) values('Max', 33, 'max@mail.ru');
+#select * from users;
+#select name, age from users where id = 2;
+#update users set name = 'Din', age = 35 where id =1;
+#delete from users where id = 2;
